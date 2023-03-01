b0df0ce59635fc576cf4650ef2819d60365435b1
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-03-01 17:46:25 -0300

    refactor: server to simplify tests

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,30 @@
 import {containsDuplicateTests, containsDuplicate }  from "./arrays_hashing/easy/contains_duplicate";
+import {validAnagramTests, isAnagram }  from "./arrays_hashing/easy/valid_anagram";
+let startTime, endTime;
 
+//executeTests(containsDuplicateTests, containsDuplicate);
 executeTests(validAnagramTests, isAnagram);
 
-containsDuplicateTests.forEach(([input, expected]) => {
-    const result = containsDuplicate(input as number[]);
-    if (result !== expected) {
-        console.log(result, expected);
-        console.error("Test failed");
-    }
-});
+console.log("\nDone\n");
 
-console.log("Done");
+function executeTests(tests: any[], func: Function) {
+    let isSuccessful = true;
+    console.log(`\n${func.name}\n`);
+    startTime = performance.now()
+    tests.forEach(([input, expected]) => {
+        const result = func(input);
+        if (result !== expected) {
+            console.log(`🔴 Test failed: "[${input}]" should be ${expected} but was ${result}\n`);
+            isSuccessful = false;
+        }
+    });
+    endTime = performance.now()
+    console.log(`Time: ${(endTime - startTime).toFixed(2)} milliseconds\n`)
+    // for (const [key,value] of Object.entries(process.memoryUsage())){
+    //     console.log(`Memory usage by ${key}, ${value/1000000} MB`)
+    // }
+    if (isSuccessful) {
+        console.log("🟢 All tests passed\n");
+    }
+    console.log("--------------------------------------------------")
+}
